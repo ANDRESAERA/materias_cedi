9f5ce22a3d0fc0ff8806f7df2a03a2d92a01273c
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:34:46 -0500

    nueva card y nueva pagina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
+import { DibujoPage } from '../dibujo/dibujo';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  historia = HistoriaPage;s
+  historia = HistoriaPage;
+  dibujo = DibujoPage;
 
   constructor(public navCtrl: NavController) {
 
   }
   clickhistoria(){
     this.navCtrl.push(this.historia);
+  }
+  clickdibujo(){
+    this.navCtrl.push(this.dibujo);
   }
 
 }
